b8f9120b6c884ffefc12d76c9d4bed2d2a541252
kuviman <kuviman@gmail.com>
2024-06-26 16:58:20 +0400

    done

---
+++ lib/lexer.ml
@@ -0,0 +1,111 @@
+(* ░▓▓██████████████████████▓▓▓▓▓▓███████████████
+                ░▓███████▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓███████████████
+                ░▓█▓█▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒▓▓▓▓▓▓▓▓▓▓▓███████████
+                ░▓█▓▓▓▓▓▓▓▓▓▓▓▓▒▓▓▓▒▒▒▒▒▒▒▒▓▓▓▓▓▓▓▓▓██████████
+                ░▓█▓▓▓▓▓▓▓▓▓▓▒▓▓▓▓▓▓▓▓▓▓▓▒▓▒▒▒▓▓▓▓▓▓██████████
+                ░▒▓▓▓▒▓▓▓▓▓▓▓▓▓▒▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▓▓▓▓▓▓█████████
+                ▒▓▓▓▓▓▓▓▓▓▓▒▒▓▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▓▓▓▓▓█████████
+               ▒▓▓██▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▓▓▓▓▒▒▒▒▒▓▓▓▓▓▓██████████
+              ▒▓▓▓██▓▓▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▓▓▓▓██████████
+            ░▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒░░░░░░░░░░░░░░░░░▒▒▒▓▓█████████
+            ▒▓▒▒▓▓▓▓▒▒▒▒▒▒▒░░░░░░              ░░░░▓▓▓████████
+           ░▓▒▒▒▒▒▒▒▒░░░░░░░░                      ░▒▓▓█████▓█
+           ▒▒▒▒▒▒▒▒▒░░░░░░░                         ░▓▓▓▓█▓▓▓▓
+           ▒▒▒▒▒▒▒▒▒░░░░░░                        ░░░░░▒▓▓█▓▓▓
+          ░▒▓▒▒▒▒▒▒▒▒░░░░░                        ░░░░░░▓█▓▓▓▓
+          ░▓▓▒▓▒▒▒▒▒▒▒░░░                   ░░░░░░░░░░▒░▒▓▓▓▓▓
+          ▒▓▓▓▓▒▒▒▒▒▒░░░ ░░░              ░░░ ░░░░░░░░▒▒▒▓▓▓▓▓
+          ▒▓▓▓▓▓▒▒▒▒▒░░░    ░░░░       ░░░░░░░░  ░░░░░▒▓▓▓████
+          ░▓▓▓▓▒▒▒▒▒▒▒░░░░▒▒░░░░░░   ░░▒▒▒▓▒▒▒▒░░░░░░▒▒▓▓█████
+           ▒▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒▓▓▓▓▒░░   ░░▒▒▒▒▒░░░░░░ ░░▒▒▒▓█████
+           ░▓▓▓▒▒▒▒▒▒▒▒▒▒░░░░░▒▒░      ░░░░░░░      ░▒░░▓█████
+           ░▓▓▓▓▒▒▒░▒░░░     ░░░                   ░░▒  ▓█████
+           ░▒▓▓▓▒▒▒▒░░░░     ░░░░                  ░░░ ░██████
+            ▒▓▓▓▓▒▒▒▒▒░░░    ░▒░                   ░░  ▒██████
+            ░▒▓▓▓▓▒▒▒▒▒░░   ░░▒░░                 ░░░  ▒██████
+             ▒▓▓▓▓▓▒▒▒▒▒░░░ ░▒▓▓▒▒░░▒▒▒          ░░▒░ ░███████
+              ░▒▓▓▓▓▒▒▒▒░░░ ░▒▒▓▓▓▒▒▒░         ░░░░▒▒▒▓███████
+               ▒▓▓▓▓▓▒▒▒░░░░░▒▒▒░▒▒▒▒░░        ░░░▒▓▓█████████
+               ▒▒▓▓▓▓▓▓▒▒░░░▒▒▒▒░░▒▒▒░░░░    ░░░░▒▓▓██████████
+               ▒▓▓▓▓▓▓▓▓▒▒▒▒▓▓▓▓▓▓▓▒▒░░░░░░░░░░▒▒▒▓▓██████████
+              ░▒▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒███▓▒▒▒▒▒░░░░▒▒▒▒▓▓▓██████████
+           ▒▒▒▒▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒░░▒▒▒▒░░░▒▒▒░░▒▒▓▓▓▓░░░▓████████
+        ░░▒▒▒▒▓▓▓▓▓███▓▓▓▒▒▒▒▓▒░░░░░░░░░▒░░▒▒▓▓▓▓▓▒░   ░▒▒▓███
+   ░░░░▒▒▒▓▓▓▓▒▒▒▓▓██▓▓▓▓▓▓▒▒▒▓▓▒▒▒▒▒░░░▒▒▒▒▓▓▓▓▓▓▒          ▒
+   ░░░░░▒▒▓▓▓▓▓▒▒█▓███▓▓▓▓▓▒▒▒▒▒▒▒▒░░░░▒▒▒▒▓▓▓▓▓▓▒      ░░
+      ░░░▒▒▓▓▓▒▒▒▓▓▓▓▓▓█▓▓▓▓▒▒▒▒▒▒░░░░░▒▒▒▓▓▓▓▓▓▒░
+      ░░░░▒▒▓▓▒▒▓▓▓▓▓▓▓██▓▓▓▓▒▒▒▒▒░░░░▒▒▓▓▓▓▓▓▓▒     ░░░░ ░░
+     ░░░▒▒▒▒▓▓▒▒▓▓▓▓▓▓▓███▓▓▓▓▒▒▒▒▒▒▒▒▓▓▓▓▓▓▓▓░        ░░░ ░░░
+        ░░░░▒▓▒▒▒▓▓▓▓▓▓█████▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒░░            ░░░
+           ░░▒▒▒▒▓▓▓▓▓▓▓▓███████████████▓▓▒▒░░               ░
+           ░░▒▒▒▒▓▓▓▓▓▓▓▓▓▓▓▓████████▓▓▓▓▒░░░░             ░░
+         ░░▒▒░▒▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒░░░░░░         ░░░░▒░
+     ░░░░░░░░▒░▒▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒░░░░░░░░    ░░░░░    ░ *)
+
+open Prelude
+
+type token = Ident of string | OpenBracket | CloseBracket
+type state = { seq : token Seq.t; start : int Option.t }
+
+let show = function Ident s -> s | OpenBracket -> "(" | CloseBracket -> ")"
+
+let single_token = function
+  | "(" -> OpenBracket
+  | ")" -> CloseBracket
+  | s -> Ident s
+
+(* pgorley: this would make more sense if it were php *)
+let parse : string -> token Seq.t =
+ fun s ->
+  let result = ref [] in
+  let start = ref None in
+  let current_pos = ref 0 in
+  let finish () =
+    match !start with
+    | Some from ->
+        result :=
+          single_token (String.sub s from (!current_pos - from)) :: !result;
+        start := None
+    | None -> ()
+  in
+  String.iteri
+    (fun i c ->
+      current_pos := i;
+      if is_space c || c = '(' || c = ')' then finish ();
+      (if not (is_space c) then
+         match !start with None -> start := Some i | _ -> ());
+      if c = '(' || c = ')' then finish ())
+    s;
+  current_pos := String.length s;
+  finish ();
+  List.to_seq (List.rev !result)
+
+let parse_old s =
+  let indices = Seq.take (String.length s) (Seq.ints 0) in
+  let finish (state : state) i =
+    match state.start with
+    | None -> state.seq
+    | Some start ->
+        Seq.append state.seq
+          (Seq.return (single_token (String.sub s start (i - start))))
+  in
+  let f (cur : state) i =
+    let c = String.get s i in
+    let finish_before = is_space c || c = '(' || c = ')' in
+    let finish_after = c = '(' || c = ')' in
+    let new_start = match is_space c with true -> None | false -> Some i in
+    let after =
+      match finish_before with
+      | true -> { seq = finish cur i; start = new_start }
+      | false -> (
+          match cur.start with
+          | Some _ -> cur
+          | None -> { seq = cur.seq; start = new_start })
+    in
+    match finish_after with
+    | true -> { seq = finish after (i + 1); start = None }
+    | false -> after
+  in
+  finish
+    (Seq.fold_left f { seq = Seq.empty; start = None } indices)
+    (String.length s)
---
+++ lib/prelude.ml
@@ -0,0 +1,40 @@
+let strip_prefix ~prefix s =
+  if String.starts_with ~prefix s then
+    let prefix_len = String.length prefix in
+    Some (String.sub s prefix_len (String.length s - prefix_len))
+  else None
+
+let strip_suffix ~suffix s =
+  if String.ends_with ~suffix s then
+    let suffix_len = String.length suffix in
+    Some (String.sub s (String.length s - suffix_len) suffix_len)
+  else None
+
+(* Copypasted from std *)
+let is_space = function ' ' | '\012' | '\n' | '\r' | '\t' -> true | _ -> false
+
+(* most readable ocaml code *)
+type split_whitespace_state = { seq : string Seq.t; start : int Option.t }
+
+(* this function will appear in my dreams tonight *)
+let split_whitespace : string -> string Seq.t =
+ fun s ->
+  let indices = Seq.take (String.length s) (Seq.ints 0) in
+  let finish (state : split_whitespace_state) i =
+    match state.start with
+    | None -> state.seq
+    | Some start ->
+        Seq.append state.seq (Seq.return (String.sub s start (i - start)))
+  in
+  let f (cur : split_whitespace_state) i =
+    let c = String.get s i in
+    match is_space c with
+    | true -> { seq = finish cur i; start = None }
+    | false -> (
+        match cur.start with
+        | Some _ -> cur
+        | None -> { seq = cur.seq; start = Some i })
+  in
+  finish
+    (Seq.fold_left f { seq = Seq.empty; start = None } indices)
+    (String.length s)
