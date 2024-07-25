41a45cde8f4b244de6ffce1b482246b14189a8a5
kuviman <kuviman@gmail.com>
2024-07-25 14:56:02 +0400

    id test

---
+++ tests/id.nix
@@ -0,0 +1,6 @@
+{ ... }:
+
+{
+	source = builtins.toFile "test.ks" "const id = forall (T :: type). ((x => x) :: T -> T); dbg <| id[int32] 123";
+	expected_output = builtins.toFile "expected" "123 :: int32\n";
+}
