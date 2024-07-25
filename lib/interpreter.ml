d679abb5082da0bd05a66dbec01d0df42290a6a4
kuviman <kuviman@gmail.com>
2024-07-25 14:55:58 +0400

    Log more info about inference fails

--- lib/interpreter.ml
+++ lib/interpreter.ml
@@ -335,170 +335,178 @@ module rec Impl : Interpreter = struct
   (* TODO if a = b then a else raise @@ Inference.FailedUnite "contexts dont match" *)
 
   and inference_unite_types (a : value_type) (b : value_type) : value_type =
-    match (a, b) with
-    (* InferVars patterns must be at the top here *)
-    | InferVar a, InferVar b ->
-        MyInference.make_same a b;
-        InferVar a
-    | InferVar var, b ->
-        MyInference.set var (Type b : value);
-        (* TODO here something can be united? should result in that union? *)
-        b
-    | a, InferVar var ->
-        MyInference.set var (Type a : value);
-        (* TODO here something can be united? should result in that union? *)
-        a
-    | Binding a, Binding b when a.id = b.id -> Binding a
-    | Binding _, _ -> failinfer ()
-    | Void, Void -> Void
-    | Void, _ -> failinfer ()
-    | UnwindToken, UnwindToken -> UnwindToken
-    | UnwindToken, _ -> failinfer ()
-    | DelimitedToken, DelimitedToken -> DelimitedToken
-    | DelimitedToken, _ -> failinfer ()
-    | Never, Never -> Never
-    | Never, _ -> failinfer ()
-    | Ast, Ast -> Ast
-    | Ast, _ -> failinfer ()
-    | Bool, Bool -> Bool
-    | Bool, _ -> failinfer ()
-    | Int32, Int32 -> Int32
-    | Int32, _ -> failinfer ()
-    | Int64, Int64 -> Int64
-    | Int64, _ -> failinfer ()
-    | Float32, Float32 -> Float32
-    | Float32, _ -> failinfer ()
-    | Float64, Float64 -> Float64
-    | Float64, _ -> failinfer ()
-    | String, String -> String
-    | String, _ -> failinfer ()
-    | BuiltinMacro, _ -> failwith "inferred builtin macro type?"
-    | Type, Type -> Type
-    | Type, _ -> failinfer ()
-    | Fn a, Fn b ->
-        Fn
-          {
-            arg_type = inference_unite_types a.arg_type b.arg_type;
-            result_type = inference_unite_types a.result_type b.result_type;
-            contexts = inference_unite_contexts a.contexts b.contexts;
-          }
-    | Fn _, _ -> failinfer ()
-    | Macro a, Macro b ->
-        Macro
-          {
-            arg_type = inference_unite_types a.arg_type b.arg_type;
-            result_type = inference_unite_types a.result_type b.result_type;
-            contexts = inference_unite_contexts a.contexts b.contexts;
-          }
-    | Macro _, _ -> failinfer ()
-    | Template a, Template b ->
-        (if a != b then
-           let compiled_a = ensure_compiled a in
-           let compiled_b = ensure_compiled b in
-           let bindings_a = pattern_bindings compiled_a.args in
-           let bindings_b = pattern_bindings compiled_b.args in
-           let vars_args : value =
-             (* TODO pattern -> value with vars in place of bindings *)
-             Dict
-               {
-                 fields =
-                   StringMap.match_map
-                     (fun _name a b : value ->
-                       let var = MyInference.new_var () in
-                       MyInference.make_same var a.value_type;
-                       MyInference.make_same var b.value_type;
-                       Var { id = Id.gen (); typ = InferVar var })
-                     bindings_a bindings_b;
-               }
-           in
-           let var_inst_a =
-             call_compiled empty_contexts (ensure_compiled a) vars_args
-           in
-           let var_inst_b =
-             call_compiled empty_contexts (ensure_compiled b) vars_args
-           in
-           ignore @@ inference_unite var_inst_a var_inst_b);
-        Template a
-    | Template _, _ -> failinfer ()
-    | NewType _, _ -> failwith "todo newtype was inferred"
-    | Dict a, Dict b ->
-        Dict
-          {
-            fields =
-              StringMap.match_map
-                (fun _name -> inference_unite_types)
-                a.fields b.fields;
-          }
-    | Dict _, _ -> failinfer ()
-    | OneOf a, OneOf b ->
-        OneOf
-          (StringMap.match_map
-             (fun _name a b ->
-               match (a, b) with
-               | None, None -> None
-               | Some a, Some b -> Some (inference_unite_types a b)
-               | None, Some _ | Some _, None -> failinfer ())
-             a b)
-    | OneOf _, _ -> failinfer ()
-    | Union _, _ -> failwith "todo union"
-    | Var a, Var b -> if a.id = b.id then Var a else failinfer ()
-    | Var _, _ -> failinfer ()
-    | MultiSet _, _ -> failwith "todo inferred multiset"
+    try
+      match (a, b) with
+      (* InferVars patterns must be at the top here *)
+      | InferVar a, InferVar b ->
+          MyInference.make_same a b;
+          InferVar a
+      | InferVar var, b ->
+          MyInference.set var (Type b : value);
+          (* TODO here something can be united? should result in that union? *)
+          b
+      | a, InferVar var ->
+          MyInference.set var (Type a : value);
+          (* TODO here something can be united? should result in that union? *)
+          a
+      | Binding a, Binding b when a.id = b.id -> Binding a
+      | Binding _, _ -> failinfer ()
+      | Void, Void -> Void
+      | Void, _ -> failinfer ()
+      | UnwindToken, UnwindToken -> UnwindToken
+      | UnwindToken, _ -> failinfer ()
+      | DelimitedToken, DelimitedToken -> DelimitedToken
+      | DelimitedToken, _ -> failinfer ()
+      | Never, Never -> Never
+      | Never, _ -> failinfer ()
+      | Ast, Ast -> Ast
+      | Ast, _ -> failinfer ()
+      | Bool, Bool -> Bool
+      | Bool, _ -> failinfer ()
+      | Int32, Int32 -> Int32
+      | Int32, _ -> failinfer ()
+      | Int64, Int64 -> Int64
+      | Int64, _ -> failinfer ()
+      | Float32, Float32 -> Float32
+      | Float32, _ -> failinfer ()
+      | Float64, Float64 -> Float64
+      | Float64, _ -> failinfer ()
+      | String, String -> String
+      | String, _ -> failinfer ()
+      | BuiltinMacro, _ -> failwith "inferred builtin macro type?"
+      | Type, Type -> Type
+      | Type, _ -> failinfer ()
+      | Fn a, Fn b ->
+          Fn
+            {
+              arg_type = inference_unite_types a.arg_type b.arg_type;
+              result_type = inference_unite_types a.result_type b.result_type;
+              contexts = inference_unite_contexts a.contexts b.contexts;
+            }
+      | Fn _, _ -> failinfer ()
+      | Macro a, Macro b ->
+          Macro
+            {
+              arg_type = inference_unite_types a.arg_type b.arg_type;
+              result_type = inference_unite_types a.result_type b.result_type;
+              contexts = inference_unite_contexts a.contexts b.contexts;
+            }
+      | Macro _, _ -> failinfer ()
+      | Template a, Template b ->
+          (if a != b then
+             let compiled_a = ensure_compiled a in
+             let compiled_b = ensure_compiled b in
+             let bindings_a = pattern_bindings compiled_a.args in
+             let bindings_b = pattern_bindings compiled_b.args in
+             let vars_args : value =
+               (* TODO pattern -> value with vars in place of bindings *)
+               Dict
+                 {
+                   fields =
+                     StringMap.match_map
+                       (fun _name a b : value ->
+                         let var = MyInference.new_var () in
+                         MyInference.make_same var a.value_type;
+                         MyInference.make_same var b.value_type;
+                         Var { id = Id.gen (); typ = InferVar var })
+                       bindings_a bindings_b;
+                 }
+             in
+             let var_inst_a =
+               call_compiled empty_contexts (ensure_compiled a) vars_args
+             in
+             let var_inst_b =
+               call_compiled empty_contexts (ensure_compiled b) vars_args
+             in
+             ignore @@ inference_unite var_inst_a var_inst_b);
+          Template a
+      | Template _, _ -> failinfer ()
+      | NewType _, _ -> failwith "todo newtype was inferred"
+      | Dict a, Dict b ->
+          Dict
+            {
+              fields =
+                StringMap.match_map
+                  (fun _name -> inference_unite_types)
+                  a.fields b.fields;
+            }
+      | Dict _, _ -> failinfer ()
+      | OneOf a, OneOf b ->
+          OneOf
+            (StringMap.match_map
+               (fun _name a b ->
+                 match (a, b) with
+                 | None, None -> None
+                 | Some a, Some b -> Some (inference_unite_types a b)
+                 | None, Some _ | Some _, None -> failinfer ())
+               a b)
+      | OneOf _, _ -> failinfer ()
+      | Union _, _ -> failwith "todo union"
+      | Var a, Var b -> if a.id = b.id then Var a else failinfer ()
+      | Var _, _ -> failinfer ()
+      | MultiSet _, _ -> failwith "todo inferred multiset"
+    with Inference.FailedUnite _ as failure ->
+      Log.error @@ "  while uniting " ^ show_type a ^ " and " ^ show_type b;
+      raise failure
 
   and inference_unite (a : value) (b : value) : value =
-    match (a, b) with
-    | Binding a, Binding b when a.id = b.id -> Binding a
-    | Binding _, _ -> failinfer ()
-    | InferVar a, InferVar b ->
-        MyInference.make_same a b;
-        InferVar a
-    | InferVar a, b ->
-        MyInference.set a b;
-        (* TODO here something can be united? should result in that union? *)
-        b
-    | a, InferVar b ->
-        MyInference.set b a;
-        (* TODO here something can be united? should result in that union? *)
-        a
-    | Var a, Var b when a.id = b.id -> Var a
-    | Var _, _ -> failinfer ()
-    | Void, Void -> Void
-    | Void, _ -> failinfer ()
-    | UnwindToken a, UnwindToken b ->
-        if a = b then UnwindToken a else failinfer ()
-    | UnwindToken _, _ -> failinfer ()
-    | DelimitedToken a, DelimitedToken b ->
-        if a = b then DelimitedToken a else failinfer ()
-    | DelimitedToken _, _ -> failinfer ()
-    | Ast a, Ast b -> if a = b then Ast a else failinfer ()
-    | Ast _, _ -> failinfer ()
-    | Macro _, _ -> failwith "inferred macro?"
-    | Function _, _ -> failwith "inferred function?"
-    | Template _, _ -> failwith "inferred template?"
-    | BuiltinMacro _, _ -> failwith "inferred builtinmacro?"
-    | BuiltinFn _, _ -> failwith "inferred builtinfn?"
-    | Bool a, Bool b -> if a = b then Bool a else failinfer ()
-    | Bool _, _ -> failinfer ()
-    | Int32 a, Int32 b -> if a = b then Int32 a else failinfer ()
-    | Int32 _, _ -> failinfer ()
-    | Int64 a, Int64 b -> if a = b then Int64 a else failinfer ()
-    | Int64 _, _ -> failinfer ()
-    | Float64 a, Float64 b -> if a = b then Float64 a else failinfer ()
-    | Float64 _, _ -> failinfer ()
-    | String a, String b -> if a = b then String a else failinfer ()
-    | String _, _ -> failinfer ()
-    | Dict { fields = a }, Dict { fields = b } ->
-        Dict
-          {
-            fields =
-              StringMap.match_map (fun _name a b -> inference_unite a b) a b;
-          }
-    | Dict _, _ -> failinfer ()
-    | Struct _, _ -> failwith "inferred struct?"
-    | Variant _, _ -> failwith "inferred variant?"
-    | Ref _, _ -> failwith "inferred ref?"
-    | Type a, Type b -> Type (inference_unite_types a b)
-    | Type _, _ -> failinfer ()
+    try
+      match (a, b) with
+      | Binding a, Binding b when a.id = b.id -> Binding a
+      | Binding _, _ -> failinfer ()
+      | InferVar a, InferVar b ->
+          MyInference.make_same a b;
+          InferVar a
+      | InferVar a, b ->
+          MyInference.set a b;
+          (* TODO here something can be united? should result in that union? *)
+          b
+      | a, InferVar b ->
+          MyInference.set b a;
+          (* TODO here something can be united? should result in that union? *)
+          a
+      | Var a, Var b when a.id = b.id -> Var a
+      | Var _, _ -> failinfer ()
+      | Void, Void -> Void
+      | Void, _ -> failinfer ()
+      | UnwindToken a, UnwindToken b ->
+          if a = b then UnwindToken a else failinfer ()
+      | UnwindToken _, _ -> failinfer ()
+      | DelimitedToken a, DelimitedToken b ->
+          if a = b then DelimitedToken a else failinfer ()
+      | DelimitedToken _, _ -> failinfer ()
+      | Ast a, Ast b -> if a = b then Ast a else failinfer ()
+      | Ast _, _ -> failinfer ()
+      | Macro _, _ -> failwith "inferred macro?"
+      | Function _, _ -> failwith "inferred function?"
+      | Template _, _ -> failwith "inferred template?"
+      | BuiltinMacro _, _ -> failwith "inferred builtinmacro?"
+      | BuiltinFn _, _ -> failwith "inferred builtinfn?"
+      | Bool a, Bool b -> if a = b then Bool a else failinfer ()
+      | Bool _, _ -> failinfer ()
+      | Int32 a, Int32 b -> if a = b then Int32 a else failinfer ()
+      | Int32 _, _ -> failinfer ()
+      | Int64 a, Int64 b -> if a = b then Int64 a else failinfer ()
+      | Int64 _, _ -> failinfer ()
+      | Float64 a, Float64 b -> if a = b then Float64 a else failinfer ()
+      | Float64 _, _ -> failinfer ()
+      | String a, String b -> if a = b then String a else failinfer ()
+      | String _, _ -> failinfer ()
+      | Dict { fields = a }, Dict { fields = b } ->
+          Dict
+            {
+              fields =
+                StringMap.match_map (fun _name a b -> inference_unite a b) a b;
+            }
+      | Dict _, _ -> failinfer ()
+      | Struct _, _ -> failwith "inferred struct?"
+      | Variant _, _ -> failwith "inferred variant?"
+      | Ref _, _ -> failwith "inferred ref?"
+      | Type a, Type b -> Type (inference_unite_types a b)
+      | Type _, _ -> failinfer ()
+    with Inference.FailedUnite _ as failure ->
+      Log.error @@ "  while uniting values " ^ show a ^ " and " ^ show b;
+      raise failure
 
   and set_ir_type (ir : ir) (t : value_type) =
     MyInference.set (ir_data ir).type_var (Type t : value)
@@ -861,7 +869,7 @@ module rec Impl : Interpreter = struct
         Union { data = same_as a; a; b }
 
   and show : value -> string = function
-    | Binding { name; _ } -> name
+    | Binding { name; id; _ } -> "<binding " ^ name ^ " " ^ Id.show id ^ ">"
     | Var { id; typ } -> "var " ^ Id.show id ^ " :: " ^ show_type typ
     | Ast ast -> "`(" ^ Ast.show ast ^ ")"
     | Variant { name; value; _ } ->
@@ -923,7 +931,7 @@ module rec Impl : Interpreter = struct
       contexts ""
 
   and show_type : value_type -> string = function
-    | Binding { name; _ } -> name
+    | Binding { name; id; _ } -> "<binding " ^ name ^ " " ^ Id.show id ^ ">"
     | UnwindToken -> "unwind_token"
     | DelimitedToken -> "delimited token"
     | Never -> "!"
