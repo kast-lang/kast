c4c6874a4711d1e780d1493abd4ad6a1e074abf1
kuviman <kuviman@gmail.com>
2024-09-30 03:15:34 +0400

    repl highlighting

--- src/main.rs
+++ src/main.rs
@@ -116,6 +116,7 @@ fn main() -> eyre::Result<()> {
             })?;
             let kast = Arc::new(Mutex::new(Kast::new()));
             let helper = {
+                use colored::Colorize;
                 struct Helper(Arc<Mutex<Kast>>);
                 struct CompletionCandidate {
                     display: String,
@@ -157,7 +158,71 @@ fn main() -> eyre::Result<()> {
                 impl rustyline::hint::Hinter for Helper {
                     type Hint = String;
                 }
-                impl rustyline::highlight::Highlighter for Helper {}
+                impl rustyline::highlight::Highlighter for Helper {
+                    fn highlight<'l>(
+                        &self,
+                        line: &'l str,
+                        _pos: usize,
+                    ) -> std::borrow::Cow<'l, str> {
+                        match kast_ast::lex(SourceFile {
+                            contents: line.to_owned(),
+                            filename: "<stdin>".into(),
+                        }) {
+                            Ok(mut reader) => {
+                                let mut result = String::new();
+                                let mut line = line.chars();
+                                let mut current_position = 0;
+                                while let Some(token) = reader.next() {
+                                    while current_position < token.span.start.index {
+                                        current_position += 1;
+                                        result.push(line.next().unwrap());
+                                    }
+                                    let colored_token = match token.token {
+                                        Token::Ident { raw, .. } => raw.underline(),
+                                        Token::Punctuation { raw, .. } => raw.normal(),
+                                        Token::String { raw, .. } => raw.green(),
+                                        Token::Number { raw, .. } => raw.italic(),
+                                        Token::Comment { raw, .. } => raw.dimmed(),
+                                        Token::Eof => break,
+                                    };
+                                    result += &colored_token.to_string();
+                                    while current_position < token.span.end.index {
+                                        current_position += 1;
+                                        line.next().unwrap();
+                                    }
+                                }
+                                result.into()
+                            }
+                            Err(_e) => line.red().to_string().into(),
+                        }
+                    }
+
+                    fn highlight_prompt<'b, 's: 'b, 'p: 'b>(
+                        &'s self,
+                        prompt: &'p str,
+                        _default: bool,
+                    ) -> std::borrow::Cow<'b, str> {
+                        prompt.bold().dimmed().to_string().into()
+                    }
+
+                    fn highlight_hint<'h>(&self, hint: &'h str) -> std::borrow::Cow<'h, str> {
+                        hint.dimmed().to_string().into()
+                    }
+
+                    fn highlight_candidate<'c>(
+                        &self,
+                        candidate: &'c str,
+                        completion: rustyline::CompletionType,
+                    ) -> std::borrow::Cow<'c, str> {
+                        let _ = completion;
+                        std::borrow::Cow::Borrowed(candidate)
+                    }
+
+                    fn highlight_char(&self, line: &str, pos: usize, forced: bool) -> bool {
+                        let _ = (line, pos, forced);
+                        true
+                    }
+                }
                 impl rustyline::validate::Validator for Helper {}
                 impl rustyline::Helper for Helper {}
                 Helper(kast.clone())
