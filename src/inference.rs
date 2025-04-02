47f3f64b8bbb49c7b238770755a3d44752804cfa
kuviman <kuviman@gmail.com>
2025-04-02 21:40:39 +0400

    preserve conflict on defaults in inference

--- src/inference.rs
+++ src/inference.rs
@@ -4,11 +4,40 @@ use rand::{thread_rng, Rng};
 
 type Check<T> = Arc<dyn Fn(&T) -> eyre::Result<CheckResult> + Send + Sync>;
 
+#[derive(Clone, Default)]
+enum DefaultValue<T> {
+    #[default]
+    None,
+    Some(Arc<T>),
+    Conflicted,
+}
+
+impl<T: Inferrable> DefaultValue<T> {
+    fn get(&self) -> Option<Arc<T>> {
+        match self {
+            DefaultValue::None | DefaultValue::Conflicted => None,
+            DefaultValue::Some(value) => Some(value.clone()),
+        }
+    }
+    fn common(a: Self, b: Self) -> Self {
+        match (a, b) {
+            (Self::None, Self::None) => Self::None,
+            (Self::None, Self::Some(value)) => Self::Some(value),
+            (Self::Some(value), Self::None) => Self::Some(value),
+            (Self::Conflicted, _) | (_, Self::Conflicted) => Self::Conflicted,
+            (Self::Some(a), Self::Some(b)) => match T::make_same(T::clone(&a), T::clone(&b)) {
+                Ok(value) => Self::Some(Arc::new(value)),
+                Err(_) => Self::Conflicted,
+            },
+        }
+    }
+}
+
 #[derive(Clone)]
 struct Data<T> {
     description: Arc<str>,
     value: Option<Arc<T>>,
-    default_value: Option<Arc<T>>,
+    default_value: DefaultValue<T>,
     checks: Vec<Check<T>>,
 }
 
@@ -221,7 +250,7 @@ impl<T: Inferrable> Var<T> {
             state: global_state::Id::new(VarState::Root(Data {
                 description: description.into(),
                 value: None,
-                default_value: None,
+                default_value: DefaultValue::None,
                 checks: Vec::new(),
             })),
         }
@@ -232,7 +261,7 @@ impl<T: Inferrable> Var<T> {
             state: global_state::Id::new(VarState::Root(Data {
                 description: description.into(),
                 value: None,
-                default_value: Some(Arc::new(default)),
+                default_value: DefaultValue::Some(Arc::new(default)),
                 checks: Vec::new(),
             })),
         }
@@ -241,7 +270,7 @@ impl<T: Inferrable> Var<T> {
         VarState::get_root(&self.state).modify(|root| {
             let root = root.as_root();
             if root.value.is_none() {
-                root.value = root.default_value.clone();
+                root.value = root.default_value.get();
                 root.run_checks()?;
             }
             Ok(root.value.clone())
@@ -288,14 +317,12 @@ impl<T: Inferrable> Var<T> {
             )?)),
         };
         let common_default_value = if common_value.is_some() {
-            None
+            DefaultValue::None
         } else {
-            match (root.default_value.take(), other_root.default_value.take()) {
-                (None, None) => None,
-                (None, Some(value)) => Some(value),
-                (Some(value), None) => Some(value),
-                (Some(a), Some(b)) => T::make_same(T::clone(&a), T::clone(&b)).ok().map(Arc::new),
-            }
+            DefaultValue::common(
+                std::mem::take(&mut root.default_value),
+                std::mem::take(&mut other_root.default_value),
+            )
         };
         if thread_rng().gen() {
             other_root.value = common_value;
