bc73c14f1fe502d1dfd7859ee86c6277ebfe1810
kuviman <kuviman@gmail.com>
2025-06-23 22:09:59 +0400

    use opam-nix

--- flake.nix
+++ flake.nix
@@ -1,91 +1,78 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    opam-repository = {
+      url = "github:ocaml/opam-repository";
+      flake = false;
+    };
     opam-nix.url = "github:tweag/opam-nix";
-    nix-filter.url = "github:numtide/nix-filter";
+    opam-nix.inputs = {
+      nixpkgs.follows = "nixpkgs";
+      opam-repository.follows = "opam-repository";
+    };
+    flake-utils.url = "github:numtide/flake-utils";
   };
-
   outputs = inputs:
-    let
-      system = "x86_64-linux";
-      pkgs = import inputs.nixpkgs { inherit system; };
-      nix-filter = inputs.nix-filter.lib;
-      opam-nix = inputs.opam-nix.lib.${system};
-      ocaml-index = pkgs.ocamlPackages.merlin.overrideAttrs (old: rec {
-        pname = "ocaml-index";
-        buildPhase = ''
-          runHook preBuild
-          dune build -p ${pname} ''${enableParallelBuilding:+-j $NIX_BUILD_CORES}
-          runHook postBuild
-        '';
-        installPhase = ''
-          runHook preInstall
-          dune install --prefix $out --libdir $OCAMLFIND_DESTDIR ${pname} \
-            --docdir $out/share/doc --mandir $out/share/man
-          runHook postInstall
-        '';
-      });
-      dune-deps = pkgs.ocamlPackages.buildDunePackage rec {
-        pname = "dune-deps";
-        version = "1.4.0";
-        src = pkgs.fetchFromGitHub {
-          owner = "mjambon";
-          repo = "dune-deps";
-          rev = "${version}";
-          sha256 = "sha256-b2bubZkyaCu9GjtAJdwtMnuuAOQUtqlU/wpw0P7chWM=";
+    let package = "kast";
+    in inputs.flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import inputs.nixpkgs { inherit system; };
+        opam-nix = inputs.opam-nix.lib.${system};
+        devPackagesQuery = {
+          # You can add "development" packages here. They will get added to the devShell automatically.
+          ocaml-lsp-server = "*";
+          ocamlformat = "*";
+          ocaml-index = "*";
+          dune-deps = "*";
         };
-        buildInputs = with pkgs.ocamlPackages; [ cmdliner sexplib ];
-        propagatedBuildInputs = with pkgs; [ graphviz ];
-      };
-      # TODO figure this out
-      opam = (opam-nix.queryToScope { } { ppx_deriving_hash = "*"; });
+        query = devPackagesQuery // {
+          ## You can force versions of certain packages here, e.g:
+          ## - force the ocaml compiler to be taken from opam-repository:
+          ocaml-base-compiler = "*";
+          ## - or force the compiler to be taken from nixpkgs and be a certain version:
+          # ocaml-system = "4.14.0";
+          ## - or force ocamlfind to be a certain version:
+          # ocamlfind = "1.9.2";
+        };
+        scope = opam-nix.buildDuneProject { } "kast" ./. query;
+        overlay = final: prev: {
+          # You can add overrides here
+          ${package} = prev.${package}.overrideAttrs (_: {
+            # Prevent the ocaml dependencies from leaking into dependent environments
+            doNixSupport = false;
+          });
+        };
+        scope' = scope.overrideScope overlay;
+        # The main package containing the executable
+        main = scope'.${package};
+        # Packages from devPackagesQuery
+        devPackages = builtins.attrValues
+          (pkgs.lib.getAttrs (builtins.attrNames devPackagesQuery) scope');
+      in {
+        legacyPackages = scope';
 
-    in {
-      packages.${system}.default = pkgs.ocamlPackages.buildDunePackage rec {
-        pname = "kast";
-        version = "0.3.0";
-        src = ./.;
-      };
-      devShells.${system}.default = pkgs.mkShell {
-        packages = let
-          ocamlDeps = with pkgs.ocamlPackages; [
-            ppxlib
-            ppx_deriving
-            ppx_blob
-            # opam.ppx_deriving_hash
-          ];
-          ocamlPackages = with pkgs.ocamlPackages; [
-            ocaml
-            ocaml-lsp
-            ocaml-index
-            dune_3
-            dune-deps
-            findlib
-            utop
-            odoc
-            ocamlformat
-          ];
-          otherPackages = with pkgs; [
-            just
+        packages.default = main;
+
+        devShells.default = pkgs.mkShell {
+          inputsFrom = [ main ];
+          buildInputs = devPackages ++ (with pkgs; [
             (pkgs.writeShellScriptBin "kast" ''
               dune exec kast "$@"
             '')
-
-            # for running js output
-            nodejs
-
-            # NIX
-            nixfmt
-            nil
-          ];
-        in ocamlDeps ++ ocamlPackages ++ otherPackages;
-        shellHook = ''
-          echo Hello from Kast devshell
-          export OCAML_BACKTRACE=1
-          export OCAMLRUNPARAM=b
-          export DUNE_CONFIG__GLOBAL_LOCK=disabled
-        '';
-      };
-      formatter.${system} = pkgs.nixfmt;
-    };
+            # You can add packages from nixpkgs here
+            just # look at .justfile
+            nodejs # for running js output
+            graphviz # for dep-graph
+            nixfmt # nix formatter
+            nil # nix lsp
+          ]);
+          shellHook = ''
+            echo Hello from Kast devshell
+            export OCAML_BACKTRACE=1
+            export OCAMLRUNPARAM=b
+            export DUNE_CONFIG__GLOBAL_LOCK=disabled
+          '';
+        };
+        formatter = pkgs.nixfmt;
+      });
 }
