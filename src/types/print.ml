b0e36cf6a5a3dc6501754d5a841aadf086128588
kuviman <kuviman@gmail.com>
2025-06-28 23:49:58 +0400

    printing all types

--- src/types/print.ml
+++ src/types/print.ml
@@ -2,7 +2,10 @@ open Std
 open Kast_util
 open Types
 
-let rec print_value_shape : formatter -> value_shape -> unit =
+let rec _unused = ()
+
+(* VALUE *)
+and print_value_shape : formatter -> value_shape -> unit =
  fun fmt -> function
   | V_Unit -> fprintf fmt "()"
   | V_Ty ty -> print_ty fmt ty
@@ -15,6 +18,7 @@ let rec print_value_shape : formatter -> value_shape -> unit =
 and print_value : formatter -> value -> unit =
  fun fmt value -> print_value_shape fmt value.shape
 
+(* TY *)
 and print_ty_shape : formatter -> ty_shape -> unit =
  fun fmt -> function
   | T_Ty -> fprintf fmt "type"
@@ -22,3 +26,51 @@ and print_ty_shape : formatter -> ty_shape -> unit =
 
 and print_ty : formatter -> ty -> unit =
  fun fmt ty -> print_ty_shape fmt ty.shape
+
+(* EXPR *)
+and print_expr_shape : formatter -> expr_shape -> unit =
+ fun fmt -> function
+  | E_Constant value -> fprintf fmt "const %a" print_value value
+  | E_Binding binding -> fprintf fmt "binding %a" print_binding binding
+  | E_Then { a; b } ->
+      fprintf fmt "then %a" (Tuple.print print_expr) (Tuple.make [ a; b ] [])
+  | E_Scope { expr } ->
+      fprintf fmt "scope %a" (Tuple.print print_expr) (Tuple.make [ expr ] [])
+  | E_Fn { arg; body } ->
+      fprintf fmt "fn (@,<0 2>@[<v>arg = %a,@]@,<0 2>@[<v>body = %a@]@ )"
+        print_pattern arg print_expr body
+  | E_Tuple { tuple } -> fprintf fmt "tuple %a" (Tuple.print print_expr) tuple
+  | E_Apply { f; arg } ->
+      fprintf fmt "apply %a" (Tuple.print print_expr)
+        (Tuple.make [] [ ("f", f); ("arg", arg) ])
+  | E_Assign { assignee; value } ->
+      fprintf fmt "assign (@ @[<v>assignee = %a,@]@ @[<v>value = %a@])"
+        print_assignee_expr assignee print_expr value
+
+and print_expr : formatter -> expr -> unit =
+ fun fmt expr -> print_expr_shape fmt expr.shape
+
+(* ASSIGNEE EXPR *)
+
+and print_assignee_expr_shape : formatter -> assignee_expr_shape -> unit =
+ fun fmt -> function
+  | A_Placeholder -> fprintf fmt "_"
+  | A_Binding binding -> fprintf fmt "binding %a" print_binding binding
+  | A_Let pattern -> fprintf fmt "let %a" print_pattern pattern
+
+and print_assignee_expr : formatter -> assignee_expr -> unit =
+ fun fmt expr -> print_assignee_expr_shape fmt expr.shape
+
+(* PATTERN *)
+
+and print_pattern_shape : formatter -> pattern_shape -> unit =
+ fun fmt -> function
+  | P_Placeholder -> fprintf fmt "_"
+  | P_Binding binding -> fprintf fmt "binding %a" print_binding binding
+
+and print_pattern : formatter -> pattern -> unit =
+ fun fmt pattern -> print_pattern_shape fmt pattern.shape
+
+(* OTHER *)
+and print_binding : formatter -> binding -> unit =
+ fun fmt binding -> fprintf fmt "%a" String.print_maybe_escaped binding.name
