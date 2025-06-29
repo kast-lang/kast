a21321e6479fe9d9a24d897eb9ddb7822ebcf8dd
Bhavya Kukkar <bhavyakukkar@gmail.com>
2025-06-29 13:24:05 +0530

    feat: highlight html
    - abstract Output with impls for Term and Html
    - option `--highlight none/term/html` for `kast fmt` cli
    - option `--html/--term` for `kast highlight` cli

--- src/fmt/cli/kast_fmt_cli.ml
+++ src/fmt/cli/kast_fmt_cli.ml
@@ -1,30 +1,64 @@
 open Std
 open Kast_util
+open Kast_highlight
 module Parser = Kast_parser
 
 module Args = struct
-  type args = { path : path }
+  type args = {
+    path : path;
+    hl_output : Kast_highlight.output option;
+  }
+
   type t = args
 
   let parse : string list -> args = function
-    | [] -> { path = Stdin }
-    | [ path ] -> { path = File path }
-    | first :: _rest -> fail "unexpected arg %S" first
+    (* stdin, no --highlight specified *)
+    | [] -> { path = Stdin; hl_output = Some Term }
+    (* file, no --highlight specified *)
+    | [ path ] -> { path = File path; hl_output = Some Term }
+    (* stdin, --highlight specified *)
+    | [ "--highlight"; "none" ] -> { path = Stdin; hl_output = None }
+    | [ "--highlight"; "html" ] -> { path = Stdin; hl_output = Some Html }
+    | [ "--highlight"; ("term" | "terminal") ] ->
+        { path = Stdin; hl_output = Some Term }
+    | [ "--highlight"; output ] ->
+        fail
+          "Unexpected highlight output '%S', only 'html', 'term' or 'none' are allowed"
+          output
+    (* file, --highlight specified *)
+    | [ "--highlight"; "none"; path ] | [ path; "--highlight"; "none" ] ->
+        { path = File path; hl_output = None }
+    | [ "--highlight"; "html"; path ] | [ path; "--highlight"; "html" ] ->
+        { path = File path; hl_output = Some Html }
+    | [ "--highlight"; ("term" | "terminal"); path ]
+    | [ path; "--highlight"; ("term" | "terminal") ] ->
+        { path = File path; hl_output = Some Term }
+    | [ "--highlight"; output; _ ] ->
+        fail
+          "Unexpected highlight output '%S', only 'html', 'term' or 'none' are allowed"
+          output
+    | first :: _rest ->
+        fail "Unexpected arg %S, expecting filename or option --highlight" first
 end
 
 let run : Args.t -> unit =
- fun { path } ->
+ fun { path; hl_output } ->
   let source = Source.read path in
   let ruleset = Kast_default_syntax.ruleset in
   let parsed = Parser.parse source ruleset in
   parsed |> Kast_fmt.format Format.str_formatter;
   let formatted = Format.flush_str_formatter () in
-  let do_highlight = true in
-  if do_highlight then
-    let parsed =
-      Parser.parse
-        { contents = formatted; filename = Special "formatted" }
-        ruleset
-    in
-    Kast_highlight.print Format.std_formatter parsed
-  else Format.printf "%s" formatted
+  match hl_output with
+  | Some output ->
+      let parsed =
+        Parser.parse
+          { contents = formatted; filename = Special "formatted" }
+          ruleset
+      in
+      let highlight_print =
+        match output with
+        | Term -> (module Term : Output)
+        | Html -> (module Html : Output)
+      in
+      Kast_highlight.print highlight_print Format.std_formatter parsed
+  | None -> Format.printf "%s" formatted
--- src/highlight/cli/kast_highlight_cli.ml
+++ src/highlight/cli/kast_highlight_cli.ml
@@ -1,21 +1,41 @@
 open Std
 open Kast_util
+open Kast_highlight
 module Lexer = Kast_lexer
 module Parser = Kast_parser
 
 module Args = struct
-  type args = { path : path }
+  type args = {
+    path : path;
+    output : output;
+  }
+
   type t = args
 
   let parse : string list -> args = function
-    | [] -> { path = Stdin }
-    | [ path ] -> { path = File path }
-    | first :: _rest -> fail "Unexpected arg %S" first
+    (* stdin *)
+    | [] -> { path = Stdin; output = Term }
+    | [ "--html" ] -> { path = Stdin; output = Html }
+    | [ ("--term" | "--terminal") ] -> { path = Stdin; output = Term }
+    (* file *)
+    | [ path ] -> { path = File path; output = Term }
+    | [ "--html"; path ] | [ path; "--html" ] ->
+        { path = File path; output = Html }
+    | [ ("--term" | "--terminal"); path ] | [ path; ("--term" | "--terminal") ]
+      ->
+        { path = File path; output = Term }
+    | _ :: first :: _rest ->
+        fail "Unexpected arg %S, expecting --html or --term" first
 end
 
 let run : Args.t -> unit =
- fun { path } ->
+ fun { path; output } ->
   let source = Source.read path in
   let lexer = Lexer.init Lexer.default_rules source in
   let parsed = Parser.parse_with_lexer lexer Kast_default_syntax.ruleset in
-  Kast_highlight.print Format.std_formatter parsed
+  let print =
+    match output with
+    | Term -> (module Term : Output)
+    | Html -> (module Html : Output)
+  in
+  Kast_highlight.print print Format.std_formatter parsed
