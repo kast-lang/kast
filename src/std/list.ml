13a599438be03e03f2fe140b62d07dd53ad9071d
kuviman <kuviman@gmail.com>
2025-07-01 01:38:01 +0400

    List.sort_by_key

--- src/std/list.ml
+++ src/std/list.ml
@@ -20,4 +20,7 @@ module List = struct
   let tail = Stdlib.List.tl
   let head = Stdlib.List.hd
   let last list = list |> rev |> head
+
+  let sort_by_key compare_key get_key =
+    sort (fun a b -> compare_key (get_key a) (get_key b))
 end
