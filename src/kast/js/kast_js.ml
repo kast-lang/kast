6afab7822555a2b366d29f67953c371da1eb9530
kuviman <kuviman@gmail.com>
2025-07-02 12:30:56 +0400

    expose changing output to js

--- src/kast/js/kast_js.ml
+++ src/kast/js/kast_js.ml
@@ -18,4 +18,31 @@ let _ =
              let expr : expr = Compiler.compile compiler Expr ast in
              let value : value = Interpreter.eval interpreter expr in
              println "%a" Value.print value
+
+       method run (source : string) =
+         let source : source =
+           { contents = source; filename = Special "source" }
+         in
+         let parsed = Parser.parse source Kast_default_syntax.ruleset in
+         match parsed.ast with
+         | None -> ()
+         | Some ast ->
+             let interpreter = Interpreter.default () in
+             let compiler = Compiler.init ~compile_for:interpreter in
+             let expr : expr = Compiler.compile compiler Expr ast in
+             let value : value = Interpreter.eval interpreter expr in
+             ignore value
+
+       method setOutput (f : string -> unit) =
+         let out_fns : Format.formatter_out_functions =
+           {
+             out_string : string -> int -> int -> unit =
+               (fun s p n -> f (String.sub s p n));
+             out_flush : unit -> unit = (fun () -> ());
+             out_newline : unit -> unit = (fun () -> f "\n");
+             out_spaces : int -> unit = (fun n -> f (String.make n ' '));
+             out_indent : int -> unit = (fun n -> f (String.make n '\t'));
+           }
+         in
+         Format.set_formatter_out_functions out_fns
     end)
