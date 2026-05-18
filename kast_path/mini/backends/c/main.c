393ee9689320afb6d64a661339fdb6e12a7387e4
kuviman <kuviman@gmail.com>
2026-05-19 00:33:23 +0400

    @context.program_args

--- kast_path/mini/backends/c/main.c
+++ kast_path/mini/backends/c/main.c
@@ -1,7 +1,9 @@
 #undef main
-int main()
+int main(int argc, char *argv[])
 {
     Context ctx;
+    ctx.program_args.data = argv;
+    ctx.program_args.length = argc;
     init_consts(&ctx);
     minikast_main.f(minikast_main.captured, &ctx);
     return 0;
